113a030df2c7806e11685384eecf5e724dc406ac
G Prasanth <35618065+prasanth-G24@users.noreply.github.com>
2020-09-02 19:57:32 +0530

    removed keys

--- telebot.js
+++ telebot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
-const token = '035018:AAHRmoKtyLUgU6WmqIxUOxhkjuGrrdRxvjY';
+const token = 'Token';
 const bot = new TelegramBot(token, {polling: true});
 /*const http = require("http");
 setInterval(function(){
